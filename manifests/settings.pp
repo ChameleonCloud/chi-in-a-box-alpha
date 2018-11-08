a78f2d6bf89910315e9c034c7785fa2f17c61af5
Jason <jasonanderson@uchicago.edu>
2018-11-08 12:08:44 -0600

    Settings cleanup
    - Comment out or remove unused settings
    - Sort settings into user-defined and ciab-defined sections
    - Add some comments about what settings do
    - Remove default values

--- manifests/settings.pp
+++ manifests/settings.pp
@@ -1,75 +1,101 @@
-$internalip = '10.20.111.250'
-$public_ip  = '129.114.97.201'
+# User-defined settings
+
+# IPv4 address on internal network
+$internalip = ''
+
+# IPv4 address on external network
+$public_ip  = ''
+
+# IP on provisioning network
+$ironic_provisioning_ip = ''
+
+# VLAN id for provisioning network
+$ironic_provisioning_vlan = ''
+
+$neutron_ngs_switches = {
+  'genericswitch:switch1' => {
+    'device_type' => 'netmiko_dell_force10',
+    'ip'          => '',
+    'username'    => '',
+    'password'    => '',
+  },
+  'genericswitch:switch2' => {
+    'device_type' => 'netmiko_dell_force10',
+    'ip'          => '',
+    'username'    => '',
+    'password'    => '',
+  }
+}
+
+# $email = '' # Email address for...
+# $region = 'CIAB' # Region name
+$physnet_interface  = ''
+
+
+##################################################################
+# NOTE: it should not be necessary to modify anything below here!
+##################################################################
+
+#
+# Default settings
+#
+
+$controller = $internalip
+$public_endpoint_url = $fqdn
 
 $db_allowed_hosts = $internalip
-$controller = $internalip
-
-$db_server = $internalip
-$public_endpoint_url = $fqdn
-$keystone_public_endpoint = "https://$public_endpoint_url:5000"
-$keystone_admin_endpoint  = "https://$public_endpoint_url:35357"
-$memcache_servers = "${internalip}:11211"
-
 $db_hammers_user = 'cc_hammers'
-$db_hammers_pass =''
-$admin_token =''
-$mysql_root =''
-$admin_password = ''
-$keystone_dbpass =''
-$glance_pass =''
-$glance_dbpass =''
-$nova_pass =''
-$nova_dbpass =''
-$nova_placement_pass =''
-$nova_placement_dbpass =''
-$neutron_pass =''
-$neutron_dbpass =''
-$metadata_proxy_shared_secret =''
-$cinder_pass =''
-$cinder_dbpass =''
-$ironic_pass =''
-$ironic_dbpass =''
-$ceilometer_pass =''
-$ceilometer_dbpass =''
-$gnocchi_pass =''
-$gnocchi_dbpass =''
-$blazar_pass =''
-$blazar_dbpass =''
-$heat_pass =''
-$heat_dbpass =''
-$heat_domain_admin_password =''
-$swift_pass =''
-$rabbit_host = $controller
-$rabbit_user = 'openstack'
-$rabbit_password =''
-$backup_password =''
-$instance_metrics_writer_password =''
-$db_readonly_pass =''
 $db_readonly_user = 'readonly'
-
-$email = 'user@host.com'
-$region = 'RegionOne'
-
-$mysql_override_options = undef
-$hammers_dbpass = ''
+$db_server = $internalip
 $instance_metrics_writer_username = 'instance_metrics_writer'
-$horizon_secret_key = ''
-$regions            = [ { 'region_name' => $region, 'blazar_dbserver' => $db_server, 'blazar_dbpass' => $blazar_dbpass, site => 'tacc_ciab' } ]
-$ironic_provisioning_vlan = '400'
-$ironic_provisioning_ip = '10.20.30.254'
-$physnet_interface  = 'p5p2'
-$neutron_ngs_switches = {
-            'genericswitch:switch1' => {
-                'device_type' => 'netmiko_dell_force10',
-                'ip'          => '1.2.3.4',
-                'username'    => 'switch_user',
-                'password'    => 'secret',
-            },
-            'genericswitch:switch2' => {
-                'device_type' => 'netmiko_dell_force10',
-                'ip'          => '1.2.3.5',
-                'username'    => 'switch_user',
-                'password'    => 'secret',
-            }
-    }
+$keystone_admin_endpoint  = "https://${public_endpoint_url}:35357"
+$keystone_public_endpoint = "https://${public_endpoint_url}:5000"
+$memcache_servers = "${internalip}:11211"
+$mysql_override_options = undef
+$rabbit_user = 'openstack'
+# $regions = [
+#   {
+#     'region_name' => $region,
+#     'blazar_dbserver' => $db_server,
+#     'blazar_dbpass' => $blazar_dbpass,
+#     'site' => 'tacc_ciab'
+#   }
+# ]
 
+#
+# The following are generated via `gensettings`
+#
+
+$admin_password = ''
+$admin_token = ''
+$backup_password = ''
+$blazar_dbpass = ''
+$blazar_pass = ''
+$ceilometer_dbpass = ''
+$ceilometer_pass = ''
+$cinder_dbpass = ''
+$cinder_pass = ''
+$db_hammers_pass = ''
+$db_readonly_pass = ''
+$glance_dbpass = ''
+$glance_pass = ''
+$gnocchi_dbpass = ''
+$gnocchi_pass = ''
+$heat_dbpass = ''
+$heat_domain_admin_password = ''
+$heat_pass = ''
+$horizon_secret_key = ''
+$instance_metrics_writer_password = ''
+$ironic_dbpass = ''
+$ironic_pass = ''
+$keystone_dbpass = ''
+$metadata_proxy_shared_secret = ''
+$mysql_root = ''
+$neutron_dbpass = ''
+$neutron_pass = ''
+$nova_dbpass = ''
+$nova_pass = ''
+$nova_placement_dbpass = ''
+$nova_placement_pass = ''
+$rabbit_password = ''
+$swift_pass = ''
