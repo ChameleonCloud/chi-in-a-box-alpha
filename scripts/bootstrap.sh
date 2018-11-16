278f361e287e81dba6a6d26da3164365c307f062
Jason <jasonanderson@uchicago.edu>
2018-11-15 18:03:42 -0600

    Use realpath

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,7 +3,7 @@ set -e -u -o pipefail
 
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-NODE_CONF="$1"
+NODE_CONF="$(realpath $1)"
 
 for f in $(find "$dir/bootstrap.d" -type f -print | sort); do
   source "$f"
